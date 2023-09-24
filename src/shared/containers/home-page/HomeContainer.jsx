0260489a19e204065d2ac6af9299270ecb4d1756
haitrieu123580 <haitrieu123580@gmail.com>
2023-09-24 16:02:02 +0700

    init project

--- src/shared/containers/home-page/HomeContainer.jsx
+++ src/shared/containers/home-page/HomeContainer.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import HomeAction from '../../../redux/home/action'
 const HomeContainer = () => {
-    const dispatch = useDispatch()
-    const handleClick = () => {
-        dispatch({
-            type: HomeAction.SAMPLE_ACTION,
-            payload: 'Something'
-        })
-    }
     return (
         <div>
             Welcome to InnSight Homepage ....
-            <button onClick={handleClick}>
-                Test redux
-            </button>
         </div>
     )
 }
